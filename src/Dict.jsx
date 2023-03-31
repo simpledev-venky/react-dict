526fd8d4fc7c8003ab06ab143c7ccad53fb9cffb
Your Name <you@example.com>
2023-03-31 17:31:25 +0530

    g-login added

--- src/Dict.jsx
+++ src/Dict.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
 import React, { useState } from 'react'
 import Card from './Card'
-    
+import dictLogo from "/src/assets/d2.png"
+import searchLogo from "/src/assets/s2.png"
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Dict = () => {
     const [word,setWord] = useState('')
+    const { loginWithRedirect,logout,isAuthenticated,user } = useAuth0();
 
     const {isLoading,isError,data,refetch} = useQuery(['meanings'],()=>
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then(res=> res.json()),
@@ -16,13 +20,29 @@ const Dict = () => {
         <h1>
       <b>W</b>andel's <b>D</b>ictionary
         </h1>
-        <img src="/src/assets/d2.png" alt="dict" className='logo' />
+        <img src={dictLogo} alt="dict" className='logo' />
+
+        {isAuthenticated ? <button className='log-btn' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>:<button className='log-btn' onClick={() => loginWithRedirect()}>Log In</button>}
+        
      </header>
+
+     {user && (
+      <div>
+        <h2 className='uname'>HI! "{user.name}"</h2>
+        <div className="upic-div">
+        <img className='upic' src={user.picture} alt={user.name} />
+        </div>
+        
+      </div>
+    ) }
+
      <div className="inp-container">
         <input type="text" value={word} placeholder='Search to Know'
         onChange={(e)=>{setWord(e.target.value)}}
         />
-        <img src="/src/assets/s2.png" alt="logo" className='logo' onClick={refetch} />
+        <img src={searchLogo} alt="logo" className='logo' onClick={refetch} />
      </div>
 
      {/* {isLoading && (<h1 className='msg'> Loading....</h1> )} */}
@@ -30,13 +50,21 @@ const Dict = () => {
      {data && console.log(data[0])}
 
      <div className="card">
-      {!data && <h2>Search to Empower yourself with knowledge.
-        "knowledge Is Divine"</h2>}
+      {!data && (
+      <>
+      <h2>Search to Empower yourself with knowledge.</h2>
+      <h2 className='quote'> "knowledge Is Divine"</h2>
+       </>
+       )}
+
      {data && (
         <Card obj={data[0]} />
         ) 
       }
-        
+
+    
+    {/* {user && console.log(user)} */}
+
      </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {QueryClient,QueryClientProvider} from "@tanstack/react-query"
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const qryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     
  <QueryClientProvider client={qryClient}>
+
+    <Auth0Provider
+    domain="dev-jvowp7i43sjvaixt.us.auth0.com"
+    clientId="Rj5TMawUQtqSIaIy7C18dUQeXWwRCrL0"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
+  </Auth0Provider>
+
  </QueryClientProvider>
 )
