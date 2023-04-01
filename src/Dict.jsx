abc9819efdbbe6208b807f29e3e029d51dd4f94d
Your Name <you@example.com>
2023-04-01 18:06:51 +0530

    ui updated,login only

--- src/Dict.jsx
+++ src/Dict.jsx
@@ -10,7 +10,7 @@ const Dict = () => {
     const [word,setWord] = useState('')
     const { loginWithRedirect,logout,isAuthenticated,user } = useAuth0();
 
-    const {isLoading,isError,data,refetch} = useQuery(['meanings'],()=>
+    const {isLoading,isError,data,refetch,error} = useQuery(['meanings'],()=>
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then(res=> res.json()),
     {enabled:false}
     )
@@ -34,30 +34,31 @@ const Dict = () => {
         <div className="upic-div">
         <img className='upic' src={user.picture} alt={user.name} />
         </div>
-        
-      </div>
-    ) }
-
-     <div className="inp-container">
+        <div className="inp-container">
         <input type="text" value={word} placeholder='Search to Know'
         onChange={(e)=>{setWord(e.target.value)}}
         />
         <img src={searchLogo} alt="logo" className='logo' onClick={refetch} />
      </div>
+      </div>
+    ) }
+
+     
 
      {/* {isLoading && (<h1 className='msg'> Loading....</h1> )} */}
-     {isError && ( <h1 className='msg'>Error</h1>) }
-     {data && console.log(data[0])}
+     {isError && ( <h1 className='msg'>{error.message}</h1>) }
+     {data && !data[0] && (<h2 className='not-found'>No Definitions Found,pls Enter Meaningful words</h2>)}
 
      <div className="card">
       {!data && (
       <>
       <h2>Search to Empower yourself with knowledge.</h2>
       <h2 className='quote'> "knowledge Is Divine"</h2>
+      {!user && ( <h2 className='center'>Log-in to Search.</h2>)}
        </>
        )}
 
-     {data && (
+     {data && !isError && data[0] && (
         <Card obj={data[0]} />
         ) 
       }
